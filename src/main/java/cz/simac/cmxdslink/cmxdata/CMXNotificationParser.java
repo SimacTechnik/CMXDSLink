785d7fed8cdf3c2fd33d58db12a006881b5e3501
Lukáš Hruška <lukyhruska96@gmail.com>
2018-11-21 10:25:10 +0100

    Checking if object is not null before printing the log

--- src/main/java/cz/simac/cmxdslink/cmxdata/CMXNotificationParser.java
+++ src/main/java/cz/simac/cmxdslink/cmxdata/CMXNotificationParser.java
@@ -86,13 +86,17 @@ public class CMXNotificationParser {
                 Class<?> cls = f.getType();
                 if(cls.isAssignableFrom(Number.class) || cls.isAssignableFrom(String.class) || cls.isAssignableFrom(Boolean.class)) {
                     CMXDSLink.LOGGER.debug("simple type");
-                    CMXDSLink.LOGGER.debug(cls.getName() + " " + f.getName() + " = " +jObj.get(f.getName()).toString() + " ("+jObj.get(f.getName()).getClass().getName()+")");
+                    CMXDSLink.LOGGER.debug(cls.getName() + " " + f.getName() + " = " +jObj.get(f.getName()) == null ? "null" : (jObj.get(f.getName()).toString() + " ("+jObj.get(f.getName()).getClass().getName()+")"));
                     f.set(obj, jObj.get(f.getName()));
                 }
                 else if(cls.isArray()) {
                     CMXDSLink.LOGGER.debug("array");
-                    CMXDSLink.LOGGER.debug(cls.getName() + " " + f.getName() + " = " +jObj.get(f.getName()).toString() + " ("+jObj.get(f.getName()).getClass().getName()+")");
+                    CMXDSLink.LOGGER.debug(cls.getName() + " " + f.getName() + " = " +jObj.get(f.getName()) == null ? "null" : (jObj.get(f.getName()).toString() + " ("+jObj.get(f.getName()).getClass().getName()+")"));
                     JSONArray arr = (JSONArray) jObj.get(f.getName());
+                    if(arr == null) {
+                        CMXDSLink.LOGGER.debug("null array");
+                        continue;
+                    }
                     CMXDSLink.LOGGER.debug("got JSONArray");
                     Object[] arrayObj = (Object[]) Array.newInstance(cls.getComponentType(), arr.size());
                     CMXDSLink.LOGGER.debug("created new array of objects");
@@ -106,9 +110,14 @@ public class CMXNotificationParser {
                 else {
                     CMXDSLink.LOGGER.debug("object: "+cls.getName());
                     try {
+                        JSONObject tmpObj = (JSONObject)jObj.get(f.getName());
+                        if(tmpObj == null) {
+                            CMXDSLink.LOGGER.debug("null complex object");
+                            continue;
+                        }
                         Object newObj = cls.getConstructor().newInstance();
                         Field[] fields = filterPublicFields(cls.getDeclaredFields());
-                        FillFields(newObj, fields, (JSONObject)jObj.get(f.getName()));
+                        FillFields(newObj, fields, tmpObj);
                     } catch (NoSuchMethodException | InstantiationException | InvocationTargetException ignore) {
                         return;
                     }
