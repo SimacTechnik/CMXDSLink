ab5ecb312ad3fd028fb224137bba382cec419676
Lukáš Hruška <lukyhruska96@gmail.com>
2018-11-23 13:13:23 +0100

    Fixed Main

--- src/main/java/cz/simac/cmxdslink/Main.java
+++ src/main/java/cz/simac/cmxdslink/Main.java
@@ -5,6 +5,6 @@ import org.dsa.iot.dslink.DSLinkFactory;
 public class Main {
 
     public static void main(String args[]) {
-        DSLinkFactory.start(new String[]{"--broker", "http://127.0.0.1:40265/conn", "-d", "../dslink.json", "--log", "debug"}, new CMXDSLink());
+        DSLinkFactory.start(args, new CMXDSLink());
     }
 }
